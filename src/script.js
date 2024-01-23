bbf947883b2963d17411403f26c804e9a172ac3b
mulurebrenda <144659497+mulurebrenda@users.noreply.github.com>
2024-01-24 00:30:05 +0300

    added the gallery section

--- src/script.js
+++ src/script.js
@@ -14,11 +14,11 @@ eventContainers.forEach((item, i) => {
   let containerDimensions = item.getBoundingClientRect();
   let containerWidth = containerDimensions.width;
 
-  nxtBtn[i].addEventListener("click", () => {
-    item.scrollLeft += containerWidth;
-  });
-
   preBtn[i].addEventListener("click", () => {
     item.scrollLeft -= containerWidth;
+  });
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
   });
 });
