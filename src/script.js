9a66e7303ef6928321c73ca500b955385e83b635
mulurebrenda <144659497+mulurebrenda@users.noreply.github.com>
2024-01-23 20:00:28 +0300

    added the events section, styled using css and use JS to make the slider arrows dynamic

--- src/script.js
+++ src/script.js
@@ -6,3 +6,19 @@ function hideSidebar() {
   const sidebar = document.querySelector("#sidebar");
   sidebar.style.display = "none";
 }
+const eventContainers = [...document.querySelectorAll(".event-container")];
+const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
+
+eventContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
